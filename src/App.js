232d71d20e9f7329971af2c1a5ec31be0a53e8b7
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-27 14:42:31 +0530

    Routes set completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './Components/Navbar';
+import Feed from './Components/Feed';
+import VideoDetail from './Components/VideoDetail';
+import ChennalDetail from './Components/ChennalDetail';
+import SearchFeed from './Components/SearchFeed';
+
 
 function App() {
+
   return (
     <div>
-      <h1>Hello Friends</h1>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element = {<Feed/>}/>
+          <Route path='/video/:id' element = {<VideoDetail/>}/>
+          <Route path='/chennal/:id' element = {<ChennalDetail/>}/>
+          <Route path='/search/:searchTerm' element = {<SearchFeed/>}/>
+        </Routes>
+        </Box>
+      </BrowserRouter>
     </div>
-  );
+  )
+
 }
 
 export default App;
