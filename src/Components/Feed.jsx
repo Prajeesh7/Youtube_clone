cbfabb337f6bf5615d80eb129a31271b78da15f4
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-28 12:53:43 +0530

    Feed section initilized

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -11,11 +11,24 @@ const Feed = ()=> {
     sx={{flexDirection: { sx: 'column', md: 'row'}}}
    >
     <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRadius:'1px solid #3d3d3d', px: {sx: 0, md: 2 }}} >
-      
+
     <SideBar />
     
     <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: ' #fff'}} >
       hello guysss
+    </Typography>
+    </Box>
+
+    <Box>
+    <Typography 
+    variant='h4' 
+    fontWeight='bold'
+    mb={2} sx={{
+      color: 'white'
+    }}
+    >
+    New <span style={{color: '#F31503'}}>Videos</span>
+
     </Typography>
     </Box>
    </Stack>
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -5,32 +5,32 @@ import { Category } from '@mui/icons-material';
 
 
 
-const selectedCategory = 'New'; 
+const selectedCategory = 'New';
 
 const SideBar = () => (
 
     <Stack direction='row'
-    sx={{ 
-    overflow:'auto',
-    height: { sx:'auto', md: '95%'},
-    flexDirection:{ md: 'column'}    
-    }} 
+        sx={{
+            overflow: 'auto',
+            height: { sx: 'auto', md: '95%' },
+            flexDirection: { md: 'column' }
+        }}
     >
-    {
-        categories.map((category)=>(
-            <button 
-            className='category-btn'
-            style={{
-                background: category.name ===
-                selectedCategory && '#FC1503',
-                color: 'white'
-            }}
-            >
-                <span style={{color: category.name === selectedCategory ? 'white' : 'red',marginRight: '15px'}}>{ category.icon}</span>
-                <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{ category.name}</span>
-            </button>
-        ))
-    }
+        {
+            categories.map((category) => (
+                <button
+                    className='category-btn'
+                    style={{
+                        background: category.name ===
+                            selectedCategory && '#FC1503',
+                        color: 'white'
+                    }}
+                >
+                    <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
+                    <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+                </button>
+            ))
+        }
 
     </Stack>
 )
