9104f99b558e680684cae2ae672745a52981532e
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-02 13:31:55 +0530

    Card Title and Chennal title set

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -18,7 +18,26 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
                 sx={{ width: 258, height: 180 }}
                 alt={ snippet?.title } 
                 />
-                <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}} />
+                <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}} >
+
+                <Link to={ videoId ? `/video/${videoId}` : demoVideoUrl }>
+                <Typography variant='subtitle1' fontWeight='bold' color='#FFF' >
+
+                    {snippet?. title.slice(0,60) || demoVideoTitle.slice(0,60) }
+
+                </Typography>    
+                </Link>
+
+
+                <Link to={ snippet.channelId ? `/channel/${ snippet?. channelId }` : demoChannelUrl }>
+                <Typography variant='subtitle2' fontWeight='bold' color='grey' >
+                
+                    { snippet?. channelTitle || demoChannelTitle }
+                    
+                </Typography>    
+                </Link>
+
+                </CardContent>
             </Link>
         </Card>
     )
