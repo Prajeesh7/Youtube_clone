80bf638170658545a3ef1b55f3e94db9d9f64cb1
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-02 11:38:41 +0530

    Video Cards initilized

---
+++ src/Components/ChennalCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChennalCard = () => {
+  return (
+    <div>
+      <h1>Chennal Videos</h1>
+    </div>
+  )
+}
+
+export default ChennalCard
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { Box, Stack } from '@mui/material'
+import VideoCard from './VideoCard'
+import ChennalCard from './ChennalCard'
+
 
 const Videos = ({ videos }) => {
 
- console.log( videos );
+  //console.log( videos );
+
 
   return (
-    <div>
-      <h2>Videos</h2>
-    </div>
+    
+    <Stack direction='row' flexWrap="wrap" justifyContent='start' gap={2}>
+
+      { videos.map((item, idx) => (
+
+        <Box key={idx} >
+          { item.id.videoId && <VideoCard video={item} />}
+          { item.id.channelId && <ChennalCard channelDetail={ item }/> }
+        </Box>
+
+      ))}
+
+    </Stack>
   )
 }
 
