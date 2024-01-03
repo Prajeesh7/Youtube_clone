133de461a8656e41889fcc6fa832f822176318cf
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-04 00:20:02 +0530

    VideoDetail initilized Before API calling

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Typography, Box, Stack } from '@mui/material'
+import ReactPlayer from 'react-player'
+import { fetchAPI } from '../Utils/fetchAPI'
+import Videos from './Videos'
+import { CheckCircle } from '@mui/icons-material'
+
+
 
 function VideoDetail() {
+
+  const { id } = useParams();
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{ xs: 'column', md: 'row' }} >
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px'}} >
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
