21b2e0b9d9f08078263d5b5bb720fd184d5f61ae
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-04 00:26:28 +0530

    Video Set in the VideoDetail

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -11,13 +11,22 @@ import { CheckCircle } from '@mui/icons-material'
 function VideoDetail() {
 
   const { id } = useParams();
+  const [videoDetail, setVideoDetail] = useState(null)
+
+  useEffect(() => {
+
+    fetchAPI(`videos?part=snippet,statistics&id=${id}`).then((data)=>{
+      setVideoDetail(data.items[0])
+    })
+  
+  }, [id])
 
   return (
     <Box minHeight='95vh'>
       <Stack direction={{ xs: 'column', md: 'row' }} >
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}} >
-            <ReactPlayer />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
           </Box>
         </Box>
       </Stack>
