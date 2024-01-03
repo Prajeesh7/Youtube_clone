d9a79298e3465ddc8448142ae2ee59632baf1d29
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-03 23:53:37 +0530

    Search Feed almost completed

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,6 +1,7 @@
 import React,{ useState,useEffect } from 'react'
 import { Box,Typography } from '@mui/material'
 import { fetchAPI } from '../Utils/fetchAPI'
+import { useParams} from 'react-router-dom'
 import  Videos  from './Videos'
 
 
@@ -9,11 +10,12 @@ import  Videos  from './Videos'
 const SearchFeed = () => {
  
  const [ videos, setVideos ] = useState([])
+ const { searchTerm } = useParams();
 
  useEffect(() => {
  
-    fetchAPI(`search?part=snippet&q=`).then((result)=>{
-        console.log(result.items)
+    fetchAPI(`search?part=snippet&q=${searchTerm}`).then((result)=>{
+        setVideos(result.items)
     })
 
  }, [])
@@ -24,7 +26,7 @@ const SearchFeed = () => {
     sx={{ overflow: 'auto', height: ' 90vh', flex:2 
     }}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white'}} >
-        Search Results for : <span style={{ color: '#F31503'}}>
+        Search Results for : {searchTerm} <span style={{ color: '#F31503'}}>
             Videos
         </span>
         </Typography>
