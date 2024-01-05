7df761b0610cb8f479ce84e9d76db61578e02a3f
Prajeesh <prajeeshrajan07@gmail.com>
2024-01-05 13:37:12 +0530

    VideoTitle $ Channel name set

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -11,22 +11,48 @@ import { CheckCircle } from '@mui/icons-material'
 function VideoDetail() {
 
   const { id } = useParams();
-  const [videoDetail, setVideoDetail] = useState(null)
+  const [videoDetails, setVideoDetails] = useState(null);
 
   useEffect(() => {
 
     fetchAPI(`videos?part=snippet,statistics&id=${id}`).then((data)=>{
-      setVideoDetail(data.items[0])
+      setVideoDetails(data.items[0])
     })
   
   }, [id])
+
+    console.log(videoDetails)
+  
+  if ( ! videoDetails ?. snippet ) return 'Loading...';
+
+  const { snippet,statistics } = videoDetails;
+
 
   return (
     <Box minHeight='95vh'>
       <Stack direction={{ xs: 'column', md: 'row' }} >
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}} >
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
+            className='react-player' controls />
+          
+          <Typography color='#fff' variant='h5' fontWeight='bold' p={2} >
+            { snippet.title }
+          </Typography>
+
+          <Stack direction='row' justifyContent='space-between' sx={{
+            color: '#fff'}} py={1} px={2} >
+
+            <Link to={`/channel/${statistics?.channelId}`} >
+
+            <Typography color='#FFF' variant={{ sm:'subtitle1', md:'h6' }} >
+              { snippet.channelTitle }
+            </Typography>
+            
+            </Link>
+
+          </Stack>
+
           </Box>
         </Box>
       </Stack>
